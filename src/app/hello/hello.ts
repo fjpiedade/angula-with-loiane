a0d8d0f6ae09b9a71394c1dd9494d005d791b8b8
fjpiedade <fernandp.joao@gmail.com>
2026-01-14 23:36:35 +0000

    start: first commit

---
+++ src/app/hello/hello.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-hello',
+  imports: [],
+  templateUrl: './hello.html',
+  styleUrl: './hello.scss',
+})
+export class Hello {
+  protected title = 'Welcome to Modern Angular! ';
+  protected isDisabled = false;
+
+  protected onClick(): void {
+    console.log('Button clicked');
+    this.isDisabled = !this.isDisabled;
+  }
+}
